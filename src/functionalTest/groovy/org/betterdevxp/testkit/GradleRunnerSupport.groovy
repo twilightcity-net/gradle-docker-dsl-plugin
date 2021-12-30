757b34466557b770ccec52ba652d671a941d55a5
Michael Lueders <mike@roadkill.org>
2021-12-30 11:20:50 -0600

    remove use of Before annotation

--- src/functionalTest/groovy/org/betterdevxp/testkit/GradleRunnerSupport.groovy
+++ src/functionalTest/groovy/org/betterdevxp/testkit/GradleRunnerSupport.groovy
@@ -2,30 +2,43 @@ package org.betterdevxp.testkit
 
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
-import org.junit.Before
 
 trait GradleRunnerSupport {
 
-    File projectDir = new File("build/gradleRunnerTestDir")
-    File buildFile = new File(projectDir, "build.gradle")
-    GradleRunner runner
+    private File projectDir = new File("build/gradleRunnerTestDir/${UUID.randomUUID()}")
+    private GradleRunner runner
 
-    @Before
-    def setupGradleRunner() {
-        projectDir.deleteDir()
-        projectDir.mkdirs()
-        new File(projectDir, "settings.gradle").text = ""
-        runner = GradleRunner.create()
-                .forwardOutput()
-                .withPluginClasspath()
-                .withProjectDir(projectDir)
+    File getProjectDir() {
+        if (projectDir.exists() == false) {
+            projectDir.mkdirs()
+        }
+        projectDir
+    }
+
+    File getBuildFile() {
+        new File(getProjectDir(), "build.gradle")
+    }
+
+    GradleRunner getRunner() {
+        if (runner == null) {
+            File settingsFile = new File(projectDir, "settings.gradle")
+            if (settingsFile.exists() == false) {
+                settingsFile.text = ""
+            }
+            projectDir.mkdirs()
+            runner = GradleRunner.create()
+                    .forwardOutput()
+                    .withPluginClasspath()
+                    .withProjectDir(projectDir)
+        }
+        runner
     }
 
     private GradleRunner runnerWithArgumentsIncludingStackTrace(String... args) {
         if ((args as List).contains("-s") == false) {
             args = args + "-s"
         }
-        runner.withArguments(args)
+        getRunner().withArguments(args)
     }
 
     BuildResult run(String... args) {
